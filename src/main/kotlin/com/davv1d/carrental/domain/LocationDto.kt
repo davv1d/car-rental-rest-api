f94bd5903cd2340399b497da275d95bf51575913
davv1d <kowalskidawid63@gmail.com>
2020-02-10 20:48:06 +0100

    Added errors messages to dto objects

--- src/main/kotlin/com/davv1d/carrental/domain/LocationDto.kt
+++ src/main/kotlin/com/davv1d/carrental/domain/LocationDto.kt
@@ -1,6 +1,10 @@
 package com.davv1d.carrental.domain
 
+import javax.validation.constraints.NotBlank
+
 class LocationDto(
         val id: Int = 0,
+        @get:NotBlank(message = "LOCATION CITY MUST NOT BE BLANK")
         val city: String = "",
+        @get:NotBlank(message = "LOCATION STREET MUST NOT BE BLANK")
         val street: String = "")
--- src/main/kotlin/com/davv1d/carrental/domain/VehicleDto.kt
+++ src/main/kotlin/com/davv1d/carrental/domain/VehicleDto.kt
@@ -1,16 +1,24 @@
 package com.davv1d.carrental.domain
 
 import java.math.BigDecimal
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 class VehicleDto(
         val id: Int = 0,
+        @get:NotBlank(message = "VEHICLE REGISTRATION MUST NOT BE BLANK")
         val registration: String,
+        @get:NotBlank(message = "VEHICLE BRAND MUST NOT BE BLANK")
         val brand: String,
+        @get:NotBlank(message = "VEHICLE MODEL MUST NOT BE BLANK")
         val model: String,
         val dailyFee: BigDecimal,
+        @get:NotNull(message = "VEHICLE LOCATION MUST NOT BE NULL")
         val locationDto: LocationDto,
+        @get:NotBlank(message = "VEHICLE BODY TYPE MUST NOT BE BLANK")
         val bodyType: String,
         val productionYear: Int,
+        @get:NotBlank(message = "VEHICLE FUEL TYPE MUST NOT BE BLANK")
         val fuelType: String,
         val power: Int
 )
