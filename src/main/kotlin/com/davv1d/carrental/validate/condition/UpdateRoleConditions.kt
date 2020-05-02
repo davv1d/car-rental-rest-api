60c3ed8ace7c1d4da73673ab20c138c25aca8ad2
davv1d <kowalskidawid63@gmail.com>
2020-05-02 23:26:24 +0200

    Change method name in role repository

--- src/main/kotlin/com/davv1d/carrental/validate/condition/UpdateRoleConditions.kt
+++ src/main/kotlin/com/davv1d/carrental/validate/condition/UpdateRoleConditions.kt
@@ -1,9 +1,7 @@
 package com.davv1d.carrental.validate.condition
 
 import com.davv1d.carrental.domain.Condition
-import com.davv1d.carrental.domain.Privilege
 import com.davv1d.carrental.domain.Role
-import com.davv1d.carrental.repository.PrivilegeRepository
 import com.davv1d.carrental.repository.RoleRepository
 import org.springframework.stereotype.Component
 
@@ -15,7 +13,7 @@ class UpdateRoleConditions(
 
     override fun get(): List<Condition<Role>> {
         val idCondition = Condition<Role>("ID DOES NOT EXIST") { role -> roleRepository.doesRoleNotExistById(role.id) }
-        val nameCondition = Condition<Role>("NAME EXISTS") { role -> roleRepository.updateCheckDoesRoleNameExist(role.name, role.id) }
+        val nameCondition = Condition<Role>("NAME EXISTS") { role -> roleRepository.doesNameExistOrNotBelongToTheUserWithThisId(role.name, role.id) }
         val privilegesConditions = rolePrivilegesConditions.get()
         return listOf(idCondition, nameCondition, privilegesConditions)
     }
