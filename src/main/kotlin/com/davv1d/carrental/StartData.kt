3bef747c06d0d2cc94f3c9e90a9b1c63a60a7b71
davv1d <kowalskidawid63@gmail.com>
2020-05-11 22:51:59 +0200

    Fix bug in startData class
    Added check if privileges exist

--- src/main/kotlin/com/davv1d/carrental/StartData.kt
+++ src/main/kotlin/com/davv1d/carrental/StartData.kt
@@ -2,53 +2,52 @@ package com.davv1d.carrental
 
 import com.davv1d.carrental.constants.*
 import com.davv1d.carrental.domain.*
-import com.davv1d.carrental.repository.LocationRepository
-import com.davv1d.carrental.repository.VehicleLocationRepository
-import com.davv1d.carrental.repository.VehicleRepository
 import com.davv1d.carrental.service.*
 import org.slf4j.LoggerFactory
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
-import java.math.BigDecimal
 import java.time.LocalDateTime
 import javax.annotation.PostConstruct
 import com.davv1d.carrental.constants.AdminAccountConfig.email
 import com.davv1d.carrental.constants.AdminAccountConfig.name
 import com.davv1d.carrental.constants.AdminAccountConfig.password
+import com.davv1d.carrental.repository.PrivilegeRepository
+import java.util.stream.Collectors
 
 @Component
-class StartData(private val privilegeService: PrivilegeService, private val roleService: RoleService, private val userService: UserService, private val passwordEncoder: PasswordEncoder
-                ,private val vehicleRepository: VehicleRepository,
-                private val vehicleLocationRepository: VehicleLocationRepository,
-                private val locationRepository: LocationRepository) {
+class StartData(private val roleService: RoleService,
+                private val userService: UserService,
+                private val passwordEncoder: PasswordEncoder,
+                private val privilegeRepository: PrivilegeRepository) {
     private val logger = LoggerFactory.getLogger(StartData::class.java)
 
     @PostConstruct
     fun createRole() {
         val privilege1 = Privilege(name = USER_REGISTRATION)
-        val privilege3 = Privilege(name = GET_PRIVILEGES)
-        val privilege4 = Privilege(name = GET_LOCATIONS)
-        val privilege5 = Privilege(name = SAVE_LOCATION)
-        val privilege6 = Privilege(name = DELETE_LOCATION)
-        val privilege7 = Privilege(name = GET_ROLES)
-        val privilege8 = Privilege(name = SAVE_ROLE)
-        val privilege9 = Privilege(name = UPDATE_OF_ROLE_PRIVILEGES)
-        val privilege10 = Privilege(name = DELETE_ROLE)
-        val privilege11 = Privilege(name = GET_LOGGED_USER)
-        val privilege12 = Privilege(name = GET_USER_BY_NAME)
-        val privilege13 = Privilege(name = GET_USER_BY_EMAIL)
-        val privilege14 = Privilege(name = GET_USERS)
-        val privilege15 = Privilege(name = DELETE_USER)
-        val privilege16 = Privilege(name = CHANGE_USER_EMAIL)
-        val privilege17 = Privilege(name = SAVE_VEHICLE)
-        val privilege18 = Privilege(name = GET_VEHICLES)
-        val privilege19 = Privilege(name = GET_VEHICLE_BY_REGISTRATION)
-        val privilege20 = Privilege(name = GET_VEHICLES_BY_BRAND)
-        val privilege21 = Privilege(name = GET_VEHICLES_BY_FUEL_TYPE)
-        val privilege22 = Privilege(name = GET_VEHICLES_BY_LOCATION)
+        val privilege2 = Privilege(name = GET_PRIVILEGES)
+        val privilege3 = Privilege(name = GET_LOCATIONS)
+        val privilege4 = Privilege(name = SAVE_LOCATION)
+        val privilege5 = Privilege(name = DELETE_LOCATION)
+        val privilege6 = Privilege(name = GET_ROLES)
+        val privilege7 = Privilege(name = SAVE_ROLE)
+        val privilege8 = Privilege(name = UPDATE_OF_ROLE_PRIVILEGES)
+        val privilege9 = Privilege(name = DELETE_ROLE)
+        val privilege10 = Privilege(name = GET_LOGGED_USER)
+        val privilege11 = Privilege(name = GET_USER_BY_NAME)
+        val privilege12 = Privilege(name = GET_USER_BY_EMAIL)
+        val privilege13 = Privilege(name = GET_USERS)
+        val privilege14 = Privilege(name = DELETE_USER)
+        val privilege15 = Privilege(name = CHANGE_USER_EMAIL)
+        val privilege16 = Privilege(name = SAVE_VEHICLE)
+        val privilege17 = Privilege(name = GET_VEHICLES)
+        val privilege18 = Privilege(name = GET_VEHICLE_BY_REGISTRATION)
+        val privilege19 = Privilege(name = GET_VEHICLES_BY_BRAND)
+        val privilege20 = Privilege(name = GET_VEHICLES_BY_FUEL_TYPE)
+        val privilege21 = Privilege(name = GET_VEHICLES_BY_LOCATION)
 
-        val allPrivileges = mutableSetOf<Privilege>().apply {
+        var allPrivileges = mutableSetOf<Privilege>().apply {
             add(privilege1)
+            add(privilege2)
             add(privilege3)
             add(privilege4)
             add(privilege5)
@@ -68,19 +67,27 @@ class StartData(private val privilegeService: PrivilegeService, private val role
             add(privilege19)
             add(privilege20)
             add(privilege21)
-            add(privilege22)
         }
 
-        val customerPrivileges = mutableSetOf<Privilege>().apply {
-            add(privilege11)
-            add(privilege16)
-            add(privilege18)
+        var customerPrivileges = mutableListOf<Privilege>().apply {
+            add(privilege10)
+            add(privilege15)
+            add(privilege17)
+            add(privilege19)
             add(privilege20)
             add(privilege21)
-            add(privilege22)
         }
-
-        privilegeService.saveAll(allPrivileges)
+        if (privilegeRepository.findAll().isNotEmpty()) {
+           allPrivileges = privilegeRepository.findAll() as MutableSet<Privilege>
+            val customerNamePrivileges = customerPrivileges.stream().map { t -> t.name }.collect(Collectors.toSet())
+            val customerPrivilegesId = allPrivileges.stream()
+                    .filter { t -> customerNamePrivileges.contains(t.name) }
+                    .map(Privilege::id)
+                    .collect(Collectors.toSet())
+            customerPrivileges = privilegeRepository.findAllById(customerPrivilegesId) as MutableList<Privilege>
+        } else {
+            privilegeRepository.saveAll(allPrivileges)
+        }
 
         val resultRoleAdmin = roleService.getRoleByName("admin")
         resultRoleAdmin.forEach(onSuccess = { logger.info("Role admin is exist") }, onFailure = { logger.info("Role admin not exist") })
@@ -91,7 +98,7 @@ class StartData(private val privilegeService: PrivilegeService, private val role
 
         val resultRoleCustomer = roleService.getRoleByName("customer")
         resultRoleCustomer.forEach(onSuccess = { logger.info("Role customer is exist") }, onFailure = { logger.info("Role customer not exist") })
-        if (resultRoleCustomer.isFailure()) roleService.save(Role(name = "customer", privileges = customerPrivileges))
+        if (resultRoleCustomer.isFailure()) roleService.save(Role(name = "customer", privileges = customerPrivileges.toSet()))
 
         val resultUserByName = userService.getUserByName("admin")
         resultUserByName.forEach(onSuccess = { logger.info("User admin is exist") }, onFailure = { logger.info("User admin not exist") })
