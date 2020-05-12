8cf08e8ae3a823edd4067699de324014f008954e
davv1d <kowalskidawid63@gmail.com>
2020-05-12 13:58:00 +0200

    Add method to location conditions
    Changed method fetchLocationRemoveConditions

--- src/main/kotlin/com/davv1d/carrental/validate/condition/LocationDbConditions.kt
+++ src/main/kotlin/com/davv1d/carrental/validate/condition/LocationDbConditions.kt
@@ -3,10 +3,14 @@ package com.davv1d.carrental.validate.condition
 import com.davv1d.carrental.domain.Condition
 import com.davv1d.carrental.domain.Location
 import com.davv1d.carrental.repository.LocationRepository
+import com.davv1d.carrental.repository.RentalRepository
+import com.davv1d.carrental.repository.VehicleLocationRepository
 import org.springframework.stereotype.Component
 
 @Component
-class LocationDbConditions(val locationRepository: LocationRepository) {
+class LocationDbConditions(val locationRepository: LocationRepository,
+                           val vehicleLocationRepository: VehicleLocationRepository,
+                           val rentalRepository: RentalRepository) {
     val LOCATION_EXIST_IN_DB = "Location exist in database"
 
     fun fetchLocationSaveConditions(): List<Condition<Location>> {
@@ -15,8 +19,9 @@ class LocationDbConditions(val locationRepository: LocationRepository) {
     }
 
     fun fetchLocationRemoveConditions(): List<Condition<Int>> {
-        val condition1 = Condition<Int>( "LOCATION WITH THIS ID NOT EXIST") { locationRepository.existsById(it) }
-        val condition2 = Condition<Int>( "IN THIS LOCATION THERE ARE CARS") { locationRepository.areThereNoVehiclesInThisLocation(it) }
-        return listOf(condition1, condition2)
+        val condition1 = Condition<Int>("LOCATION WITH THIS ID NOT EXIST") { locationRepository.existsById(it) }
+        val condition2 = Condition<Int>("IN THIS LOCATION THERE ARE CARS") { vehicleLocationRepository.isLocationNotUsedInVehicleLocation(it) }
+        val condition3 = Condition<Int>("Location is used in rental") { rentalRepository.isLocationNotUsedInRental(it) }
+        return listOf(condition1, condition2, condition3)
     }
 }
