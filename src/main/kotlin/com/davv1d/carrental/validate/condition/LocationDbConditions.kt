590cc01769f1d89ccce37e9b053c11f7081fd1f8
davv1d <kowalskidawid63@gmail.com>
2020-05-12 11:34:26 +0200

    Change location conditions methods

--- src/main/kotlin/com/davv1d/carrental/validate/condition/LocationDbConditions.kt
+++ src/main/kotlin/com/davv1d/carrental/validate/condition/LocationDbConditions.kt
@@ -10,14 +10,13 @@ class LocationDbConditions(val locationRepository: LocationRepository) {
     val LOCATION_EXIST_IN_DB = "Location exist in database"
 
     fun fetchLocationSaveConditions(): List<Condition<Location>> {
-        val con1 = Condition<Location>(LOCATION_EXIST_IN_DB)
-        { location -> with(location) { locationRepository.doesLocationExist(city, street) } }
+        val con1 = Condition<Location>(LOCATION_EXIST_IN_DB) { l -> locationRepository.doesLocationNotExist(l.city, l.street) }
         return listOf(con1)
     }
 
     fun fetchLocationRemoveConditions(): List<Condition<Int>> {
-        val condition1 = Condition<Int>( "LOCATION WITH THIS ID NOT EXIST") { locationRepository.doesLocationNotExistById(it) }
-        val condition2 = Condition<Int>( "IN THIS LOCATION THERE ARE CARS") { locationRepository.areThereVehiclesInLocation(it) }
+        val condition1 = Condition<Int>( "LOCATION WITH THIS ID NOT EXIST") { locationRepository.existsById(it) }
+        val condition2 = Condition<Int>( "IN THIS LOCATION THERE ARE CARS") { locationRepository.areThereNoVehiclesInThisLocation(it) }
         return listOf(condition1, condition2)
     }
 }
