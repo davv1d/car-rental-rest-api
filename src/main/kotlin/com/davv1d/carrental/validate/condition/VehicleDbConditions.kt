779f76cc31deb36d91f6715c87318eec9b080907
davv1d <kowalskidawid63@gmail.com>
2020-05-12 12:03:29 +0200

    Change vehicle conditions methods

--- src/main/kotlin/com/davv1d/carrental/validate/condition/VehicleDbConditions.kt
+++ src/main/kotlin/com/davv1d/carrental/validate/condition/VehicleDbConditions.kt
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Component
 class VehicleDbConditions(private val vehicleRepository: VehicleRepository) {
 
     fun fetchVehicleSaveConditions(): List<Condition<Vehicle>> {
-        val condition1 = Condition<Vehicle>("REGISTRATION NUMBER IS EXIST") { vehicle -> vehicleRepository.doesRegistrationExist(vehicle.registration) }
+        val condition1 = Condition<Vehicle>("REGISTRATION NUMBER IS EXIST") { vehicle -> vehicleRepository.doesRegistrationNotExist(vehicle.registration) }
         return listOf(condition1)
     }
 
     fun fetchVehicleUpdateConditions(): List<Condition<Vehicle>> {
-        val condition1 = Condition<Vehicle>("ID DOES NOT EXIST") { vehicle -> vehicleRepository.doesIdNotExist(vehicle.id) }
-        val condition2 = Condition<Vehicle>("REGISTRATION NUMBER DOES EXIST") { v -> vehicleRepository.doesRegistrationExistOrNotBelongToTheVehicleWithThisId(v.registration, v.id) }
+        val condition1 = Condition<Vehicle>("ID DOES NOT EXIST") { vehicle -> vehicleRepository.existsById(vehicle.id) }
+        val condition2 = Condition<Vehicle>("REGISTRATION NUMBER DOES EXIST") { v -> vehicleRepository.doesRegistrationNotExistOrBelongToTheVehicleWithThisId(v.registration, v.id) }
         return listOf(condition1, condition2)
     }
 
