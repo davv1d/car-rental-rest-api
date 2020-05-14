23d6e77660b05d83a1b481d5c9d0288fe8b6b63d
davv1d <kowalskidawid63@gmail.com>
2020-05-14 18:28:22 +0200

    Refactor location repository
    Remove not used methods and change name

--- src/main/kotlin/com/davv1d/carrental/repository/LocationRepository.kt
+++ src/main/kotlin/com/davv1d/carrental/repository/LocationRepository.kt
@@ -10,10 +10,6 @@ import javax.transaction.Transactional
 @Repository
 @Transactional
 interface LocationRepository : CrudRepository<Location, Int> {
-    @Query(value = "select case when count(l) > 0 then true else false end from Location l where " +
-            "upper(l.city) like upper(:city) and upper(l.street) like upper(:street)")
-    fun doesLocationExist(city: String, street: String): Boolean
-
     override fun findAll(): List<Location>
 
     @Query(value = "select case when count(l) = 0 then true else false end from Location l where " +
@@ -26,17 +22,4 @@ interface LocationRepository : CrudRepository<Location, Int> {
     @Query(value = "select l from Location l where " +
             "upper(l.city) like upper(:city) and upper(l.street) like upper(:street)")
     fun findLocation(city: String, street: String): Location?
-
-    @Query(value = "select case when count(l) = 0 then true else false end " +
-            "from Location l where l.id = :id")
-    fun doesLocationNotExistById(id: Int): Boolean
-
-    @Query(nativeQuery = true,
-            value = "select case when count(*) > 0 then 'true' else 'false' end from VEHICLES as V where V.LOCATION_ID like :id")
-    fun areThereVehiclesInLocation(id: Int): Boolean
-
-    @Query(nativeQuery = true,
-            value = "select case when count(*) = then 'true' else 'false' end from VEHICLES as V where V.LOCATION_ID like :id")
-    fun areThereNoVehiclesInThisLocation(id: Int): Boolean
-
 }
